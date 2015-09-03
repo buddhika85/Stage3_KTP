dac7e5f69192daa9ccc3a4cc78be40a1131c86df
buddhika85 <buddhika2006system@gmail.com>
2015-09-03 10:29:59 +0100

    Limited the number of decimal places input to two in the PPI input of negotiation popup

--- bcmy.stock/BCMY.Stock.Client/common/Util.js
+++ bcmy.stock/BCMY.Stock.Client/common/Util.js
@@ -102,4 +102,47 @@
             return false;
         return true;
     }
+
+    // Limit decimal places to 2
+    // After the decimal point key press, this will return false if user tries to input three decimal points
+    // Ref - http://stackoverflow.com/questions/23221557/restrict-to-2-decimal-places-in-keypress-of-a-text-box and 
+    // http://jsfiddle.net/S9G8C/203/
+    function validateFloatKeyPressToTwoDecimalPlaces(el, evt) {
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        
+        // make backspace functional
+        if (charCode != 8) {
+            var number = el.value.split('.');
+            if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+                return false;
+            }
+            //just one dot
+            if (number.length > 1 && charCode == 46) {
+                return false;
+            }
+            //get the carat position
+            var caratPos = getSelectionStart(el);
+            var dotPos = el.value.indexOf(".");
+            if (caratPos > dotPos && dotPos > -1 && (number[1].length > 1)) {
+                return false;
+            }
+            return true;
+        }
+        else {
+            return true;
+        }        
+    }
+
+    // Ref - http://stackoverflow.com/questions/23221557/restrict-to-2-decimal-places-in-keypress-of-a-text-box and 
+    // http://jsfiddle.net/S9G8C/203/
+    //thanks: http://javascript.nwbox.com/cursor_position/
+    function getSelectionStart(o) {
+        if (o.createTextRange) {
+            var r = document.selection.createRange().duplicate()
+            r.moveEnd('character', o.value.length)
+            if (r.text == '') return o.value.length
+            return o.value.lastIndexOf(r.text)
+        } else return o.selectionStart
+    }
+
 //}());
