cd8d9f8fb6ca29b5c24b2c906b67c6ee3b6424b5
buddhika85 <buddhika2006system@gmail.com>
2015-12-08 10:14:54 +0000

    MOQ - unit testing - started but not complete

--- bcmy.stock/BCMY.WebAPI/Util/OrderLineNegotiationValidator.cs
+++ bcmy.stock/BCMY.WebAPI/Util/OrderLineNegotiationValidator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess_EF.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -28,5 +29,51 @@ namespace BCMY.WebAPI.Util
             }
             return isValid;
         }
-    }
+
+
+        /// <summary>
+        /// Orderline validation
+        /// </summary>
+        public static bool ValidateOrderLineOrNegotiation(TblOrderLine orderLine)
+        {
+            bool isValid = false;
+            try
+            {
+                isValid = GeneralValidator.IsNumeric(orderLine.productId) &&
+                    GeneralValidator.IsDecimalNumber(orderLine.quantity) &&
+                    GeneralValidator.IsDecimalNumber(orderLine.negotiatedPricePerItem) &&
+                    GeneralValidator.IsDecimalNumber(orderLine.totalAmount) &&
+                    GeneralValidator.IsStringNotEmpty(orderLine.status) &&
+                    GeneralValidator.IsNumeric(orderLine.orderId);
+            }
+            catch (Exception)
+            {
+                return isValid;
+            }
+            return isValid;
+        }
+
+        /// <summary>
+        /// Negotiation validation
+        /// </summary>
+        public static bool ValidateOrderLineOrNegotiation(TblNegotiation negotiation)
+        {
+            bool isValid = false;
+            try
+            {
+                isValid = GeneralValidator.IsNumeric(negotiation.productId) &&
+                    GeneralValidator.IsDecimalNumber(negotiation.quantity) &&
+                    GeneralValidator.IsDecimalNumber(negotiation.negotiatedPricePerItem) &&
+                    GeneralValidator.IsDecimalNumber(negotiation.totalAmount) &&
+                    GeneralValidator.IsStringNotEmpty(negotiation.status) &&
+                    GeneralValidator.IsNumeric(negotiation.orderId);
+            }
+            catch (Exception)
+            {
+                return isValid;
+            }
+            return isValid;
+        }
+
+    }    
 }
